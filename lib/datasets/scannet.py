d73593e0c4911d530162e3e8ba3b392866fe10f6
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-12-28 18:29:43 +0800

    correct id

--- lib/datasets/scannet.py
+++ lib/datasets/scannet.py
@@ -188,6 +188,7 @@ class ScannetVoxelizationDataset(VoxelizationDataset):
             cat_path = instance_folder + sample_cat
             print(cat_path)
             print(os.listdir(cat_path))
+            print(random.choice(os.listdir(cat_path)))
             file = cat_path + '/' + random.choice(os.listdir(cat_path))
             inst_coords, inst_feats, inst_labels, instance_ids, _ = self.load_ply_w_path(file, scene_name)
 

1d651e21f249a6430b7de325f67a60ec45a03078
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-12-29 04:21:51 +0800

    ensemble done

--- run_ensemble.py
+++ run_ensemble.py
@@ -4,43 +4,36 @@ from tqdm import tqdm, trange
 from collections import Counter
 from tqdm import tqdm
 
-list_1 = [1,2,3]
-list_2 = [4,5,6]
 
-# Create an empty list
-list_ = []
+# read result for a txt file
+def read_txt(path):
+    preds = []
+    with open(path, 'rb') as f:
+        lines = f.readlines()
 
-# Create List of lists
-list_.append(list_1)
-list_.append(list_2)
-print (list_)
-print(list_[1])
-print(list_[1][1])
+        for p in lines:
+            p = int(str(p, encoding="utf-8").rstrip('\n'))
+            preds.append(p)
+
+    return preds
 
 # read txt files
 def read_path(path):
     filenames = []
     with open(path, 'rb') as f:
         lines = f.readlines()
+
         for line in lines:
             line = str(line, encoding="utf-8").rstrip('\n')
             line = line.split("/")[1].split(".")[0] + ".txt"
             filenames.append(line)
-    return filenames, len(filenames)
 
-# read result for a file
-def read_txt(path):
-    preds = []
-    with open(path, 'rb') as f:
-        lines = f.readlines()
-        for p in lines:
-            p = int(str(p, encoding="utf-8").rstrip('\n'))
-            preds.append(p)
-    return preds
+    return filenames, len(filenames)
 
 # read results for a folder
 def get_pred(txt_path, filenames):
     pred_list = []
+    num_list = []
     #print(filenames)
 
     for filename in tqdm(filenames):
@@ -50,9 +43,10 @@ def get_pred(txt_path, filenames):
         #     print(p, end=" ")
         # print(len(preds))
         pred_list.append(preds)
+        num_list.append(len(preds))
         
     # print("num of file:", len(pred_list))
-    return pred_list
+    return pred_list, num_list
 
 def voting(numbers):
     counts = Counter(numbers)
@@ -76,7 +70,7 @@ def voting(numbers):
 if __name__=="__main__":
     
     filenames, num_files = read_path("./scannet_200/test.txt")
-    txt_paths = ["./submit_tail_continue_001/","./submit_tail_continue_001/"]
+    txt_paths = ["./submit_tail_continue_001/","./submit_tail6033_002/", ]
     outpath = "./ensemble"
     os.makedirs(outpath, exist_ok=True)
 
@@ -84,9 +78,12 @@ if __name__=="__main__":
 
     for txt_path in txt_paths:
         print(f"Loading txts from file {txt_path}")
-        pred_list = get_pred(txt_path, filenames)
+        pred_list, num_list = get_pred(txt_path, filenames)
         elective.append( pred_list )
 
+    print(len(num_list))
+    print(num_list[0])
+    
     # print(elective)
     # print(elective[0]) # pred_list, final_pred_list
     # print(elective[0][0]) # preds, final_preds
@@ -95,20 +92,23 @@ if __name__=="__main__":
     print("Voting...")
     final_pred_list = []
     for i in trange(num_files):
-        for pred_list in elective:
+        final_preds = []
+        for j in range(num_list[i]):
             #print(pred_list)
-            final_preds = []
-            for preds in pred_list:
-                vote_box = []
-                for p_i in preds:
-                    vote_box.append(p_i)
-                p = voting(vote_box)
+            vote_box = []
+            for pred_list in elective:
+                vote_box.append(pred_list[i][j]) # 3
+                # print(len(vote_box))
+
+            p = voting(vote_box)
+            # print(vote_box, "=>", p)
             final_preds.append(p)
         final_pred_list.append(final_preds)
 
     print("Saving result txts...")
     for i, filename in enumerate(tqdm(filenames)):
-        preds = np.array(final_pred_list[i])
+        final_preds = np.array(final_pred_list[i])
+        # print(final_preds)
         # print(len(preds))
         save_as = os.path.join(outpath, filename)
-        np.savetxt(save_as, preds, fmt='%i')
+        np.savetxt(save_as, final_preds, fmt='%i')

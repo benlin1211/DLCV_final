6f5f68806652a1a84df617fede9168b38649ebfe
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-12-29 03:41:45 +0800

    vote v2

--- ensemble.py
+++ ensemble.py
@@ -2,33 +2,47 @@ import os
 import numpy as np
 from tqdm import tqdm, trange
 from collections import Counter
+from tqdm import tqdm
 
+list_1 = [1,2,3]
+list_2 = [4,5,6]
+
+# Create an empty list
+list_ = []
+
+# Create List of lists
+list_.append(list_1)
+list_.append(list_2)
+print (list_)
+print(list_[1])
+print(list_[1][1])
+
+# read txt files
 def read_path(path):
     filenames = []
     with open(path, 'rb') as f:
         lines = f.readlines()
-
         for line in lines:
             line = str(line, encoding="utf-8").rstrip('\n')
             line = line.split("/")[1].split(".")[0] + ".txt"
             filenames.append(line)
     return filenames, len(filenames)
 
-from tqdm import tqdm
+# read result for a file
 def read_txt(path):
-    filenames = []
+    preds = []
     with open(path, 'rb') as f:
         lines = f.readlines()
+        for p in lines:
+            p = int(str(p, encoding="utf-8").rstrip('\n'))
+            preds.append(p)
+    return preds
 
-        for line in lines:
-            line = int(str(line, encoding="utf-8").rstrip('\n'))
-            filenames.append(line)
-    return filenames
-
-
+# read results for a folder
 def get_pred(txt_path, filenames):
     pred_list = []
     #print(filenames)
+
     for filename in tqdm(filenames):
         path = os.path.join(txt_path, filename)
         preds = read_txt(path)
@@ -36,7 +50,7 @@ def get_pred(txt_path, filenames):
         #     print(p, end=" ")
         # print(len(preds))
         pred_list.append(preds)
-        # breakpoint()
+        
     # print("num of file:", len(pred_list))
     return pred_list
 
@@ -59,34 +73,42 @@ def voting(numbers):
 # def most_common(lst):
 #     return max(set(lst), key=lst.count)
 
-filenames, num_files = read_path("./scannet_200/test.txt")
-txt_paths = ["./submit_tail_continue_001/","./submit_tail_continue_001/"]
-outpath = "./ensemble"
-os.makedirs(outpath, exist_ok=True)
+if __name__=="__main__":
+    
+    filenames, num_files = read_path("./scannet_200/test.txt")
+    txt_paths = ["./submit_tail_continue_001/","./submit_tail_continue_001/"]
+    outpath = "./ensemble"
+    os.makedirs(outpath, exist_ok=True)
 
-elective = []
+    elective = []
 
-for txt_path in txt_paths:
-    print(f"Loading txts from file {txt_path}")
-    pred_list = get_pred(txt_path, filenames)
-    print(len(pred_list))
-    elective.append( pred_list )
-    print(len(elective))
+    for txt_path in txt_paths:
+        print(f"Loading txts from file {txt_path}")
+        pred_list = get_pred(txt_path, filenames)
+        elective.append( pred_list )
 
-print("Voting...")
-final_pred_list = []
-for i in trange(num_files):
-    vote_box = []
-    for pred_list in elective:
-        #print(pred_list)
-        vote_box.append(pred_list[i])
+    # print(elective)
+    # print(elective[0]) # pred_list, final_pred_list
+    # print(elective[0][0]) # preds, final_preds
+    # print(elective[0][0][0]) # p_i, p
 
-    p = voting(vote_box)
-    final_pred_list.append(p)
+    print("Voting...")
+    final_pred_list = []
+    for i in trange(num_files):
+        for pred_list in elective:
+            #print(pred_list)
+            final_preds = []
+            for preds in pred_list:
+                vote_box = []
+                for p_i in preds:
+                    vote_box.append(p_i)
+                p = voting(vote_box)
+            final_preds.append(p)
+        final_pred_list.append(final_preds)
 
-print("Saving result txts...")
-for i, filename in enumerate(tqdm(filenames)):
-    preds = np.array(final_pred_list[i])
-    # print(len(preds))
-    save_as = os.path.join(outpath, filename)
-    np.savetxt(save_as, preds, fmt='%i')
+    print("Saving result txts...")
+    for i, filename in enumerate(tqdm(filenames)):
+        preds = np.array(final_pred_list[i])
+        # print(len(preds))
+        save_as = os.path.join(outpath, filename)
+        np.savetxt(save_as, preds, fmt='%i')

27200769fc3831d5ae22cafe9c5f5a47bfed1b3b
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-12-29 21:09:08 +0800

    upload

--- inference.sh
+++ inference.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+
+
 source scripts/test_sample_tail.sh Res16UNet34C 1 sample_tail --seed=1211 
+
+<Path to test_data folder> <Path to test.txt folder> <Path to output .txt file>
 
 # cd ./output/Scannet200Voxelization2cmDataset/Res16UNet34C-sample_tail/visualize/fulleval
 # zip submit_final.zip *.txt

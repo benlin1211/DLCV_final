e0c0585b21e5f7bb68a213bd3962943fea0e90d7
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-12-29 23:06:20 +0800

    modify inference

--- inference.sh
+++ inference.sh
@@ -8,15 +8,17 @@ bash get_dataset.sh
 get_ckpt.sh
 
 # Copy your txt file to my folder
-cp "$1/test.txt" ./scannet_200
+cp $1/test.txt ./scannet_200
 
 # Copy your ply folder to my folder
-cp -r "$2/test/" ./scannet_200
+cp -r $2/test/ ./scannet_200
 
 source scripts/test_sample_tail.sh Res16UNet34C 1 sample_tail --seed=1211 
 
-mkdir output_mock
-mv "./output/Scannet200Voxelization2cmDataset/Res16UNet34C-sample_tail/visualize/fulleval/*" "./{$3}"
+# rm -rf $3
+# mkdir $3
+# cp -r "./output/Scannet200Voxelization2cmDataset/Res16UNet34C-sample_tail/visualize/fulleval/*.txt" "./$3"
+cp ./output/Scannet200Voxelization2cmDataset/Res16UNet34C-sample_tail/visualize/fulleval/*.txt ./$3
 # 
 # zip submit_final.zip *.txt
 # cp ./submit_final.zip ../..

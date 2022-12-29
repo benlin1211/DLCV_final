396374567102abe5f5b1f8318d353ae7e51e8cd1
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2022-12-29 21:37:19 +0800

    inference w/o model ckpt

---
+++ get_dataset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Download dataset
+gdown [] -O final_data.zip
+
+# Unzip the downloaded zip file
+unzip ./final_data.zip 
--- inference.sh
+++ inference.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# download and upzip dataset
+bash get_datast.sh
 
+# download model
+# TODO
+
+# Copy your txt to my folder
+cp "{$1}/test.txt" ./scannet_200
+
+# Copy your txt to my folder
+cp -r "{$2}/test/" ./scannet_200
 
 source scripts/test_sample_tail.sh Res16UNet34C 1 sample_tail --seed=1211 
 
-<Path to test_data folder> <Path to test.txt folder> <Path to output .txt file>
-
-# cd ./output/Scannet200Voxelization2cmDataset/Res16UNet34C-sample_tail/visualize/fulleval
+mv "./output/Scannet200Voxelization2cmDataset/Res16UNet34C-sample_tail/visualize/fulleval/*" "./{$3}"
+# 
 # zip submit_final.zip *.txt
 # cp ./submit_final.zip ../..
 # rm -rf ./submit_final.zip
 # cd ../../../../..
+
+# bash inference.sh ./scannet_200_mock ./scannet_200_mock ./output_mock
